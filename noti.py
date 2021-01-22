f11f33adc38c4e1f328c7f6360b89a3fa08c6e29
Alex Kelly <alex.kelly@franklin.edu>
2021-01-22 10:25:30 -0500

    Code formatting and comments

--- noti.py
+++ noti.py
@@ -7,11 +7,18 @@ import select
 import click
 from config import Config
 
-cf = Config()
 __version__ = "0.0.1"
+
+cf = Config() # Config object we will use globally for options
 
 
 def login(username, password):
+    """
+    Attempt to login to the homeserver with the username and password specified
+    :param username:
+    :param password:
+    :return: requests object
+    """
     base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
     login = requests.post(
         f"{base}/login",
@@ -25,29 +32,45 @@ def login(username, password):
 
 
 def send_message(token, messagetext):
+    """
+    Sends the message specified to your configured homeserver
+    :param token: Authtoken that is qcquired from 'login'
+    :param messagetext: text of the message you wish to send
+    :return:  requests object
+    """
     base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
-    roomid = urllib.parse.quote(cf.config['room']['id'])
-    roomurl = (
-        f"{base}/rooms/{roomid}/send/m.room.message"
-        f"?access_token=" + token
-    )
+    roomid = urllib.parse.quote(cf.config["room"]["id"])
+    roomurl = f"{base}/rooms/{roomid}/send/m.room.message" f"?access_token=" + token
     message = requests.post(roomurl, json={"msgtype": "m.text", "body": messagetext})
     return message
 
 
 @click.command()
-@click.option('-m', '--message', help="Message text to send")
-@click.option('-c', '--config', type=click.Path(),
-              help="Alternate path to config file",
-              default="./config.yaml")
-@click.option('--version', type=click.BOOL, is_flag=True, help="Display version information")
+@click.option("-m", "--message", help="Message text to send")
+@click.option(
+    "-c",
+    "--config",
+    type=click.Path(),
+    help="Alternate path to config file",
+    default="./config.yaml",
+)
+@click.option(
+    "--version", type=click.BOOL, is_flag=True, help="Display version information"
+)
 def main(message, config, version):
+    """
+    Main program logic
+    :param message:  click option
+    :param config:  click option
+    :param version: click option
+    :return: None
+    """
     if version:
         print(__version__)
         sys.exit()
     cf.load_config(config)
-    mylogin = login(cf.config['user']['name'], cf.config['user']['pass'])
-    token = mylogin.json()['access_token']
+    mylogin = login(cf.config["user"]["name"], cf.config["user"]["pass"])
+    token = mylogin.json()["access_token"]
     # I don't really understand what select does here, but it's used to figure
     # out if we have data from stdin or not
     if select.select(
