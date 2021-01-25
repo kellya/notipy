22980cb9f1f8c8d92d1a3c6be4a642c27e0c0e5c
Alex Kelly <alex.kelly@franklin.edu>
2021-01-25 12:21:48 -0500

    Added token retrieval, send command, token-based login through Authorization header

--- noti.py
+++ noti.py
@@ -3,50 +3,17 @@
 import requests
 import urllib.parse
 import sys
+
 import select
 import click
 from config import Config
 
 __version__ = "0.0.1"
 
-cf = Config() # Config object we will use globally for options
+cf = Config()  # Config object we will use globally for options
 
 
-def login(username, password):
-    """
-    Attempt to login to the homeserver with the username and password specified
-    :param username:
-    :param password:
-    :return: requests object
-    """
-    base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
-    login = requests.post(
-        f"{base}/login",
-        json={
-            "type": "m.login.password",
-            "user": username,
-            "password": password,
-        },
-    )
-    return login
-
-
-def send_message(token, messagetext):
-    """
-    Sends the message specified to your configured homeserver
-    :param token: Authtoken that is qcquired from 'login'
-    :param messagetext: text of the message you wish to send
-    :return:  requests object
-    """
-    base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
-    roomid = urllib.parse.quote(cf.config["room"]["id"])
-    roomurl = f"{base}/rooms/{roomid}/send/m.room.message" f"?access_token=" + token
-    message = requests.post(roomurl, json={"msgtype": "m.text", "body": messagetext})
-    return message
-
-
-@click.command()
-@click.option("-m", "--message", help="Message text to send")
+@click.group(name="main", invoke_without_command=True)
 @click.option(
     "-c",
     "--config",
@@ -57,33 +24,70 @@ def send_message(token, messagetext):
 @click.option(
     "--version", type=click.BOOL, is_flag=True, help="Display version information"
 )
-def main(message, config, version):
-    """
-    Main program logic
-    :param message:  click option
-    :param config:  click option
-    :param version: click option
-    :return: None
-    """
+def main(config, version):
     if version:
         print(__version__)
         sys.exit()
     cf.load_config(config)
-    mylogin = login(cf.config["user"]["name"], cf.config["user"]["pass"])
-    token = mylogin.json()["access_token"]
-    # I don't really understand what select does here, but it's used to figure
-    # out if we have data from stdin or not
-    if select.select(
-        [
-            sys.stdin,
-        ],
-        [],
-        [],
-        0.0,
-    )[0]:
-        send_message(token, sys.stdin.readline())
-    else:
-        send_message(token, message)
+
+    # if select.select(
+    #    [
+    #        sys.stdin,
+    #    ],
+    #    [],
+    #    [],
+    #    0.0,
+    #)[0]:
+    #    send(sys.stdin.readline())
+
+
+@main.command()
+@click.option("-u", "--user", prompt=True)
+@click.option(
+    "-p", "--password", prompt=True, confirmation_prompt=False, hide_input=True
+)
+def token(user, password):
+    """Get an auth token for the user"""
+    base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
+    login = requests.post(
+        f"{base}/login",
+        json={
+            "type": "m.login.password",
+            "user": user,
+            "password": password,
+        },
+    )
+    try:
+        token = login.json()["access_token"]
+    except KeyError:
+        print("Username/password combination is not valid")
+        sys.exit()
+    print(token)
+
+
+@main.command()
+@click.option("--roomid", prompt=True)
+def join(roomid):
+    print(f"join {roomid}")
+
+
+@main.command()
+@click.argument("messagetext")
+def send(messagetext):
+    "Send a message to your alert room defined in config.yaml"
+    base = cf.config["homeserver"]["base"] + cf.config["homeserver"]["api_base"]
+    roomid = urllib.parse.quote(cf.config["room"]["id"])
+    roomurl = f"{base}/rooms/{roomid}/send/m.room.message"
+    message = requests.post(
+        roomurl,
+        json={
+            "msgtype": "m.text",
+            "body": messagetext,
+        },
+        headers={
+            "Authorization": "Bearer " + cf.config["user"]["token"],
+        },
+    )
 
 
 if __name__ == "__main__":
