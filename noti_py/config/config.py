a7608ebaf6f1970189e55ce7f76b06dbda15d4c0
Alex Kelly <kellya@arachnitech.com>
2021-08-27 16:57:13 -0400

    refactor: code cleanup for not specifying a configfile default and encoding

--- noti_py/config/config.py
+++ noti_py/config/config.py
@@ -1,21 +1,21 @@
+""" Create a config object to manage configuration details """
 import yaml
 
-""" Create a config object to manage configuration details """
 
-
-class Config:
+class Config:  # pylint: disable=too-few-public-methods
     """The config class to hold config details"""
 
     def __init__(self):
         self.config = None
 
-    def load_config(self, configfile):
+    def load_config(self, configfile="conf.yaml"):
         """Load the configuration from specified file"""
         try:
-            with open(configfile, "r") as config_file:
+            with open(configfile, "r", encoding="UTF-8") as config_file:
                 config_entries = yaml.safe_load(config_file)
         except FileNotFoundError as error:
             print(error)
             return False
         else:
             self.config = config_entries
+        return True
